0428445620acb606c366c0715e82898409d90543
farbod29 <aprin.farbod@gmail.com>
2024-04-19 11:31:28 +0200

    changing month in Farakhor
    Signed-off-by: farbod29 <aprin.farbod@gmail.com>

--- src/app/Farakhor/page.tsx
+++ src/app/Farakhor/page.tsx
@@ -44,49 +44,73 @@ const occasionsData: Occasions = {
     '۲۸': 'روز بزرگداشت حکیم عمر خیام',
     '۲۹': 'روز جهانی موزه و میراث فرهنگی [ 18 May ]',
   },
+  خرداد: {},
+  تیر: {},
+  مرداد: {},
+  شهریور: {},
+  مهر: {},
+  آبان: {},
+  آذر: {},
+  دی: {},
+  بهمن: {},
+  اسفند: {},
 };
 
 const Occasions: React.FC = () => {
-  const [currentMonthEvents, setCurrentMonthEvents] = useState<{
-    [key: string]: string;
-  }>({});
-  const [currentMonthName, setCurrentMonthName] = useState('');
+  const [currentMonthEvents, setCurrentMonthEvents] = useState<Occasion>({});
+  const [currentMonthName, setCurrentMonthName] = useState<string>('');
+  const monthNames = [
+    'فروردین',
+    'اردیبهشت',
+    'خرداد',
+    'تیر',
+    'مرداد',
+    'شهریور',
+    'مهر',
+    'آبان',
+    'آذر',
+    'دی',
+    'بهمن',
+    'اسفند',
+  ];
 
   useEffect(() => {
     const today = new Date();
     const jToday = jalaali.toJalaali(today);
-    const monthNames = [
-      'فروردین',
-      'اردیبهشت',
-      'خرداد',
-      'تیر',
-      'مرداد',
-      'شهریور',
-      'مهر',
-      'آبان',
-      'آذر',
-      'دی',
-      'بهمن',
-      'اسفند',
-    ];
-
-    const currentMonthName = monthNames[jToday.jm - 1];
-    setCurrentMonthEvents(occasionsData[currentMonthName]);
-    setCurrentMonthName(currentMonthName);
-    console.log(currentMonthName);
+    updateMonth(jToday.jm - 1);
   }, []);
 
+  const updateMonth = (monthIndex: number) => {
+    const newName = monthNames[monthIndex];
+    setCurrentMonthEvents(occasionsData[newName]);
+    setCurrentMonthName(newName);
+  };
+
+  const handleNextMonth = () => {
+    const currentMonthIndex = monthNames.indexOf(currentMonthName);
+    const nextMonthIndex = (currentMonthIndex + 1) % monthNames.length;
+    updateMonth(nextMonthIndex);
+  };
+
+  const handlePreviousMonth = () => {
+    const currentMonthIndex = monthNames.indexOf(currentMonthName);
+    const previousMonthIndex =
+      (currentMonthIndex - 1 + monthNames.length) % monthNames.length;
+    updateMonth(previousMonthIndex);
+  };
+
   return (
-    <div className=" bg-[#1CD2D5] flex flex-col">
-      <div className="bg-[#FF7F50] shadow rounded-lg p-12 w-full text-center text-2xl text-bold text-white fixed mr-8">
-        <h1 className="align-center items-center">{`فراخورهای ماه فروردین`}</h1>
+    <div className="bg-[#1CD2D5] flex flex-col">
+      <div className="bg-[#FF7F50] shadow rounded-lg p-12 w-full text-center text-2xl text-bold text-white fixed mr-8 flex justify-between items-center">
+        <button onClick={handlePreviousMonth}>&lt;</button>
+        <h1>{`فراخورهای ماه ${currentMonthName}`}</h1>
+        <button onClick={handleNextMonth}>&gt;</button>
       </div>
       <div className="flex flex-col items-center mt-36">
-        {/* Adjust margin-top as necessary */}
         {Object.entries(currentMonthEvents).map(([day, event]) => (
           <div
             key={day}
-            className="bg-white shadow rounded-lg p-4 mb-4 w-full md:w-1/2 text-right" // Added text-right here
+            className="bg-white shadow rounded-lg p-4 mb-4 w-full md:w-1/2 text-right"
           >
             <div className="text-gray-700">
               <div className="font-semibold">{day}</div>
