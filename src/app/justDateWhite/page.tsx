ad35cef34795980b9faa734e8cbea294ba12956f
Farbod <aprin.farbod@gmail.com>
2024-05-16 23:41:08 +0200

    adjust white text for just date !
    Signed-off-by: Farbod <aprin.farbod@gmail.com>

--- src/app/justDateWhite/page.tsx
+++ src/app/justDateWhite/page.tsx
@@ -63,15 +63,15 @@ export default function Home() {
   return (
     <main className="flex min-h-screen w-full flex-col items-center px-2 sm:px-4 md:px-8">
       <div className="w-full max-w-4xl mx-auto mt-2">
-        <div className=" justify-center items-center text-center text-xl md:text-4xl lg:text-5xl text-[#ffffff] flex flex-row-reverse">
-          <div className="pl-4">{dates.day}</div>
-          <div>{dates.monthName}</div>
-          <br />
-        </div>
         <div className="text-center p-4 rounded-xl flex justify-center items-center">
-          <div className="text-center pl-4 text-[#ffffff] text-2xl justify-center items-center">
+          <div className="text-center text-[#ffffff] text-xl justify-center items-center p-0">
             {dates.weekday}
           </div>
+        </div>
+        <div className="justify-center items-center text-center text-3xl md:text-2xl lg:text-2xl text-[#ffffff] flex flex-row-reverse -mt-4">
+          <div className="p-1">{dates.day}</div>
+          <div>{dates.monthName}</div>
+          <br />
         </div>
       </div>
     </main>
