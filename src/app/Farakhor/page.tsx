90f3d7fcadb9bd792888c72b195afa2644f2329b
Farbod <aprin.farbod@gmail.com>
2024-05-17 17:05:06 +0200

    remove back of farakhor
    Signed-off-by: Farbod <aprin.farbod@gmail.com>

--- src/app/Farakhor/page.tsx
+++ src/app/Farakhor/page.tsx
@@ -130,7 +130,7 @@ const Occasions: React.FC = () => {
   const todayGregorian = getTodayGregorian(); // Call this outside the render loop to avoid recomputation
 
   return (
-    <div className="bg-white flex flex-col items-center justify-center pt-24">
+    <div className=" flex flex-col items-center justify-center pt-24">
       <div className="bg-[#FF8200] shadow-lg rounded-lg px-4 py-7 w-full text-center text-xl md:text-2xl font-bold text-white fixed top-0 flex justify-between items-center z-10">
         <button
           onClick={() =>
