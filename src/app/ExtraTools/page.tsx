b969dbdb230b48d39d62fbc7e0ce5934ff70fd66
Farbod <aprin.farbod@gmail.com>
2024-06-19 11:35:38 +0200

    put text and icons of extra tools vertically in middle same y
    Signed-off-by: Farbod <aprin.farbod@gmail.com>

--- src/app/ExtraTools/page.tsx
+++ src/app/ExtraTools/page.tsx
@@ -21,9 +21,9 @@ const ExtraTools = () => {
 
   return (
     <main className='flex flex-col items-center min-h-screen px-2 py-3 bg-[#333863] md:px-8 pb-12'>
-      <div className='w-full max-w-md space-y-4 py-3 p-2'>
+      <div className='w-full max-w-md space-y-3 py-3 p-2'>
         <div
-          className='bg-[#7F84B4] rounded-2xl p-9 text-white text-end block'
+          className='bg-[#7F84B4] rounded-2xl p-3 text-white flex items-center justify-between h-20'
           onClick={() => handleOpenModal('تماس ')}
         >
           <Image
@@ -32,25 +32,25 @@ const ExtraTools = () => {
             width={40}
             height={40}
           />
-          تماس با گاه‌شمار
+          <span>تماس با گاه‌شمار</span>
         </div>
         <Link
           href='https://www.instagram.com/gahshomar.iran/?igsh=MW52MHcwcWpnY2QyNA%3D%3D'
           legacyBehavior
           passHref
         >
-          <a className='bg-[#7F84B4] rounded-2xl p-9 text-white text-end block'>
+          <a className='bg-[#7F84B4] rounded-2xl p-3 text-white flex items-center justify-between h-20'>
             <Image
               src={'/assets/insta.png'}
               alt='گاه نامه'
               width={40}
               height={40}
             />
-            اینستاگرام
+            <span>اینستاگرام</span>
           </a>
         </Link>
         <div
-          className='bg-[#7F84B4] rounded-2xl p-11 text-white text-end'
+          className='bg-[#7F84B4] rounded-2xl p-3 text-white flex items-center justify-between h-20'
           onClick={() => handleOpenModal(<ClocksPageMobile />)}
         >
           <Image
@@ -59,21 +59,21 @@ const ExtraTools = () => {
             width={40}
             height={40}
           />
-          ساعت های جهانی
+          <span>ساعت های جهانی</span>
         </div>
         <Link href='/gahshomaranDark' legacyBehavior passHref>
-          <a className='bg-[#7F84B4] rounded-2xl p-9 text-white text-end block'>
+          <a className='bg-[#7F84B4] rounded-2xl p-3 text-white flex items-center justify-between h-20'>
             <Image
-              src={'/assets/clock.png'}
+              src={'/assets/calendaerIcon.png'}
               alt='سرآغازها'
               width={40}
               height={40}
             />
-            سرآغازها
+            <span>سرآغازها</span>
           </a>
         </Link>
         <div
-          className='bg-[#7F84B4] rounded-2xl p-9 text-white text-end'
+          className='bg-[#7F84B4] rounded-2xl p-3 text-white flex items-center justify-between h-20'
           onClick={() => handleOpenModal('تبدیل تاریخ')}
         >
           <Image
@@ -82,7 +82,7 @@ const ExtraTools = () => {
             width={40}
             height={40}
           />
-          تبدیل تاریخ
+          <span>تبدیل تاریخ</span>
         </div>
 
         <div
