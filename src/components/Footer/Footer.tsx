155aa35f7fb2c05e462a2bbfb1ab10c5c29e5c57
Farbod <aprin.farbod@gmail.com>
2024-08-28 22:38:58 +0200

    again roll back to test exist full screen icon on vercel
    Signed-off-by: Farbod <aprin.farbod@gmail.com>

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,90 +1,5 @@
-// // src/components/Footer/Footer.tsx
-// 'use client';
-// import Image from 'next/image';
-
-// import Link from 'next/link';
-// import { usePathname } from 'next/navigation';
-// import Icon from '../FooterIcons/FooterIcons';
-// import { useState } from 'react';
-// import FullscreenIcon from './FullscreenIcon';
-// import ExitFullscreenIcon from './ExitFullscreenIcon';
-
-// const Footer = () => {
-//   const pathname = usePathname();
-//   const [isFullscreen, setIsFullscreen] = useState(false);
-
-//   const buttons = [
-//     { path: '/', label: '', icon: 'home' },
-//     { path: '/smallCalendarMobile', label: '', icon: 'calendar' },
-//     {
-//       path: '/FarakhorMobileDark',
-//       label: '',
-//       icon: 'FarakhorMobileDark',
-//     },
-//     { path: '/AiGenerator', label: '', icon: 'AiGenerator' },
-//     { path: '/ExtraTools', label: '', icon: 'ExtraTools' },
-//   ];
-
-//   const toggleFullscreen = () => {
-//     if (!document.fullscreenElement) {
-//       document.documentElement.requestFullscreen().catch((err) => {
-//         console.error(
-//           `Error attempting to enable full-screen mode: ${err.message}`
-//         );
-//       });
-//       setIsFullscreen(true);
-//     } else {
-//       document.exitFullscreen().catch((err) => {
-//         console.error(
-//           `Error attempting to disable full-screen mode: ${err.message}`
-//         );
-//       });
-//       setIsFullscreen(false);
-//     }
-//   };
-
-//   return (
-//     <footer className='fixed bottom-0 w-full p-4 flex justify-around items-center bg-[#373D70]'>
-//       {buttons.map((button) => (
-//         <Link
-//           href={button.path}
-//           key={button.path}
-//           className='flex flex-col items-center text-center w-1/5'
-//         >
-//           <Icon
-//             name={button.icon}
-//             className={`w-6 h-6 mb-1 ${
-//               pathname === button.path ? 'text-[#FD821D]' : 'text-white'
-//             }`}
-//           />
-//           <span
-//             className={`text-xs bg ${
-//               pathname === button.path ? 'text-[#FD821D]' : 'text-white'
-//             }`}
-//           >
-//             {button.label}
-//           </span>
-//         </Link>
-//       ))}
-//       <button
-//         onClick={toggleFullscreen}
-//         className='flex flex-col items-center text-center w-1/5 pb-0  '
-//       >
-//         {isFullscreen ? <ExitFullscreenIcon /> : <FullscreenIcon />}
-//       </button>
-//     </footer>
-//   );
-// };
-
-// export default Footer;
-
-// // src/components/Footer/Footer.tsx
-// 'use client';
-// import Image from 'next/image';
-
 import FullscreenIcon from './FullscreenIcon';
 import ExitFullscreenIcon from './ExitFullscreenIcon';
-import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import Icon from '../FooterIcons/FooterIcons';
@@ -93,6 +8,18 @@ import { useState } from 'react';
 const Footer = () => {
   const pathname = usePathname();
   const [isFullscreen, setIsFullscreen] = useState(false);
+
+  const buttons = [
+    { path: '/', label: '', icon: 'home' },
+    { path: '/smallCalendarMobile', label: '', icon: 'calendar' },
+    {
+      path: '/FarakhorMobileDark',
+      label: '',
+      icon: 'FarakhorMobileDark',
+    },
+    { path: '/AiGenerator', label: '', icon: 'AiGenerator' },
+    { path: '/ExtraTools', label: '', icon: 'ExtraTools' },
+  ];
 
   const toggleFullscreen = () => {
     if (!document.fullscreenElement) {
@@ -114,9 +41,30 @@ const Footer = () => {
 
   return (
     <footer className='fixed bottom-0 w-full p-4 flex justify-around items-center bg-[#373D70]'>
+      {buttons.map((button) => (
+        <Link
+          href={button.path}
+          key={button.path}
+          className='flex flex-col items-center text-center w-1/5'
+        >
+          <Icon
+            name={button.icon}
+            className={`w-6 h-6 mb-1 ${
+              pathname === button.path ? 'text-[#FD821D]' : 'text-white'
+            }`}
+          />
+          <span
+            className={`text-xs ${
+              pathname === button.path ? 'text-[#FD821D]' : 'text-white'
+            }`}
+          >
+            {button.label}
+          </span>
+        </Link>
+      ))}
       <button
         onClick={toggleFullscreen}
-        className='flex flex-col items-center text-center w-1/5 pb-0  '
+        className='flex flex-col items-center text-center w-1/5 pb-0'
       >
         {isFullscreen ? <ExitFullscreenIcon /> : <FullscreenIcon />}
       </button>
