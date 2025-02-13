326dd289be0260c2ff54eb2ea0f7fa3f0ca06638
Farbod Aprin <Aprin.farbod@gmail.com>
2025-02-13 12:58:47 +0100

    try to fix server bug 3
    Signed-off-by: Farbod Aprin <Aprin.farbod@gmail.com>

--- src/app/api/occasions/[id]/route.ts
+++ src/app/api/occasions/[id]/route.ts
@@ -1,18 +1,16 @@
-import { ObjectId } from 'mongodb';
 import { NextRequest, NextResponse } from 'next/server';
-import clientPromise from '@/lib/mongodb';
+import prisma from '@/lib/prisma';
 
 export async function GET(
-  req: NextRequest,
+  request: NextRequest,
   { params }: { params: { id: string } }
 ) {
   try {
-    const client = await clientPromise;
-    const db = client.db('farakhor');
-    const collection = db.collection('farakhorCollection');
-
-    const occasion = await collection.findOne({
-      _id: new ObjectId(params.id),
+    const id = params.id;
+    const occasion = await prisma.occasion.findUnique({
+      where: {
+        id: parseInt(id),
+      },
     });
 
     if (!occasion) {
@@ -26,7 +24,55 @@ export async function GET(
   } catch (error) {
     console.error('Error fetching occasion:', error);
     return NextResponse.json(
-      { error: 'Failed to fetch occasion' },
+      { error: 'Internal Server Error' },
+      { status: 500 }
+    );
+  }
+}
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const id = params.id;
+    const data = await request.json();
+
+    const updatedOccasion = await prisma.occasion.update({
+      where: {
+        id: parseInt(id),
+      },
+      data,
+    });
+
+    return NextResponse.json(updatedOccasion);
+  } catch (error) {
+    console.error('Error updating occasion:', error);
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 }
+    );
+  }
+}
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const id = params.id;
+
+    await prisma.occasion.delete({
+      where: {
+        id: parseInt(id),
+      },
+    });
+
+    return NextResponse.json({ message: 'Occasion deleted successfully' });
+  } catch (error) {
+    console.error('Error deleting occasion:', error);
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
       { status: 500 }
     );
   }
